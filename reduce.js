c2a6ee769b7a2dde3eade10d75721ebf41908c1f
Sadanand Gopal <sadanandgpl@gmail.com>
2022-07-10 12:47:35 +0530

    config added

--- reduce.js
+++ reduce.js
@@ -1,26 +1,37 @@
-const products = [
-  { name: 'apples', category: 'fruits' },
-  { name: 'oranges1', category: 'fruits' },
-  { name: 'oranges2', category: 'fruits' },
+// const products = [
+//   { name: 'apples', category: 'fruits' },
+//   { name: 'oranges1', category: 'fruits' },
+//   { name: 'oranges2', category: 'fruits' },
 
-  { name: 'oranges2', category: 'dryFRUITS' },
-  { name: 'oranges2', category: 'dryFRUITS' },
-  { name: 'oranges2', category: 'dryFRUITS' },
-  { name: 'oranges2', category: 'dryFRUITS' },
+//   { name: 'oranges2', category: 'dryFRUITS' },
+//   { name: 'oranges2', category: 'dryFRUITS' },
+//   { name: 'oranges2', category: 'dryFRUITS' },
+//   { name: 'oranges2', category: 'dryFRUITS' },
 
+//   { name: 'potatoes', category: 'vegetables' },
+//   { name: 'potatoes1', category: 'vegetables' },
+//   { name: 'potatoes2', category: 'vegetables' },
+//   { name: 'potatoes3', category: 'vegetables' },
+//   { name: 'potatoes4', category: 'vegetables' },
+// ];
 
-  { name: 'potatoes', category: 'vegetables' },
-  { name: 'potatoes1', category: 'vegetables' },
-  { name: 'potatoes2', category: 'vegetables' },
-  { name: 'potatoes3', category: 'vegetables' },
-  { name: 'potatoes4', category: 'vegetables' },
+// const sop = products.reduce((group, el) => {
+//   const { category } = el;
+//   group[category] = group[category] ?? 0;
+//   group[category] += 1;
+//   return group;
+// }, {});
+
+const persons = [
+  { id: 1, name: "A", salary: 1200 },
+  { id: 2, name: "B", salary: 1500 },
+  { id: 3, name: "c", salary: 1500 },
 ];
 
-
-const sop = products.reduce((group, el) => {
-  const { category } = el;
-    group[category]  = group[category] ?? 0;
-    group[category] += 1;
-  return group;
+const aw = persons.reduce((obj, item) => {
+  const { id } = item;
+  obj[id] = item;
+  return obj;
 }, {});
- console.log(sop)
+
+console.log(aw);
